cbc91d546a1379197b4b7850d50d16c9d943cebc
Orbserver <53362977+Orbserver@users.noreply.github.com>
2024-02-15 11:02:26 +0100

    Update TPassGen.py

--- TPassGen.py
+++ TPassGen.py
@@ -1,15 +1,15 @@
-init = 2018
-end = 2024
+Dstart = 2018
+Dend = 2024
 
 def passwdPattern(user_inpput):
     comb = []
 
-    for year in range(init, end + 1):
+    for year in range(Dstart, Dend + 1):
         comb.append(f"{user_input}{year}")
 
     for i in range(len(user_input)):
         for j in range(i, len(user_input)):
-            for year in range(init, end + 1):
+            for year in range(Dstart, Dend + 1):
                 comb.append(f"{user_input[:i]}{user_input[i:j+1].upper()}{user_input[j+1:]}{year}")
 
     string = "\n".join(comb)
